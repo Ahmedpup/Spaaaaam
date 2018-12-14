33a04117f574dfcc167838176f62022f8bf6a863
Ahmedpup <45870421+Ahmedpup@users.noreply.github.com>
2018-12-14 14:46:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,4 +10,36 @@ channel.send(`** spaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 }, 30)
 })
 
+client.on('message', message => {
+	 var prefix = "1-";
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+// -say
+  if (command === "say") {
+          message.delete()
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+  }
+  
+ 
+
+if (command == "embed") {
+    let say = new Discord.RichEmbed()
+  .setThumbnail(message.author.avatarURL)  
+  .setAuthor(message.author.username)
+    .setDescription(args.join("  "))
+    .setColor(0x06DF00)
+    message.channel.sendEmbed(say);
+    message.delete();
+  }
+  
+
+
+});
+
 client.login("NTIzMTI4MTM3NDY3NDI4ODY1.DvVBBg.kLwbq0X0brl0IcqzhjOpwBGX57E");
