65761df282d595ee62514c14838ffe8c772cb662
Ahmedpup <45870421+Ahmedpup@users.noreply.github.com>
2018-12-14 14:39:27 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("523130993234804740")
+setInterval(function() {
+channel.send(`** spaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaam **`);
+}, 30)
+})
+
+client.login("NTIzMTI4MTM3NDY3NDI4ODY1.DvVBBg.kLwbq0X0brl0IcqzhjOpwBGX57E");
